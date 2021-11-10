b9d5fbe21bdf566b1aa1659bbf79d410a96b11d9
Alexander Slavschik <slavshik@me.com>
2021-11-11 00:37:34 +0300

    fix (index.ts): colors and params

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 let CURRENT_LOG_LEVEL = 2; // error level by default
-
 type LogLevelSetting = [
     tag: string,
     colors: [background: string] | [background: string, foreground: string],
@@ -8,52 +7,47 @@ type LogLevelSetting = [
 
 const settings: {[level: number]: LogLevelSetting} = {
     [1]: ["fatal", ["#7c002a"], console.error],
-    [2]: ["info", ["#4454FF", "#273095"], console.info],
-    [3]: ["error", ["#f31", "#f00"], console.error],
-    [4]: ["warn", ["#ffcd84", "#ff5b0c"], console.warn],
-    [5]: ["debug", ["#115816"], console.log],
+    [2]: ["info", ["#4454FF", "#7e86de"], console.info],
+    [3]: ["error", ["#f31"], console.error],
+    [4]: ["warn", ["#ffcd84"], console.warn],
+    [5]: ["debug", ["#168d21", "#168d21"], console.log],
     [6]: ["trace", ["#aaa"], console.log]
 };
-
+const typeToPrintf = (item: any) => {
+    switch (typeof item) {
+        case "string":
+            return "%s";
+        case "boolean":
+            return "%o";
+        case "number":
+            return (0 ^ item) === item ? "%i" : "%f";
+        default:
+            return "%O";
+    }
+};
 const tryToLog =
     (level: number, scope = "") =>
-        (...params: any[]) => {
+        (params: any[]) => {
             if (CURRENT_LOG_LEVEL >= level) {
                 if (settings[level]) {
-                    const [tag, colors, logFunction] = settings[level];
-                    const idd: string[] = params.map(item => {
-                        switch (typeof item) {
-                            case "string":
-                                return "%s";
-                            case "boolean":
-                                return "%o";
-                            case "number":
-                                return (0 ^ item) === item ? "%i" : "%f";
-                            default:
-                                return "%O";
-                        }
-                    });
-
-                    logFunction.apply(
-                        null,
-                        [
-                            `%s %c #%s %c ${idd.join(" ")}`,
-                            scope,
-                            `background-color:${colors[0] ? colors[0] : "color:white"}`,
-                            tag,
-                            colors[1] ? `color: ${colors[1]}` : ""
-                        ].concat(...params)
-                    );
+                    const [tag, colors, log_fn] = settings[level];
+                    log_fn.apply(null, [
+                        `${scope} %c #%s %c ${params.map(typeToPrintf).join(" ")}`,
+                        `background-color:${colors[0] || ""};color:white;`,
+                        tag,
+                        colors[1] ? `color:${colors[1]};` : "",
+                        ...params
+                    ]);
                 }
             }
         };
-const scope = (scopeName?: string) => ({
-    trace: (...params: unknown[]) => tryToLog(6, scopeName)(params),
-    debug: (...params: unknown[]) => tryToLog(5, scopeName)(params),
-    warn: (...params: unknown[]) => tryToLog(4, scopeName)(params),
-    error: (...params: unknown[]) => tryToLog(3, scopeName)(params),
-    info: (...params: unknown[]) => tryToLog(2, scopeName)(params),
-    fatal: (...params: unknown[]) => tryToLog(1, scopeName)(params)
+const scope = (name?: string) => ({
+    trace: (...params: unknown[]) => tryToLog(6, name)(params),
+    debug: (...params: unknown[]) => tryToLog(5, name)(params),
+    warn: (...params: unknown[]) => tryToLog(4, name)(params),
+    error: (...params: unknown[]) => tryToLog(3, name)(params),
+    info: (...params: unknown[]) => tryToLog(2, name)(params),
+    fatal: (...params: unknown[]) => tryToLog(1, name)(params)
 });
 export const log = {
     ...scope(),
